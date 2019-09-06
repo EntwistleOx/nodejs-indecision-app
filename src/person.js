2b84cd05bc911ce6f1c2beef2ce9cc19612937d9
Juan Díaz <juan.diaz.12069@gmail.com>
2019-09-06 18:32:23 -0400

    webpack, babel-loader, babel-core

---
+++ src/person.js
@@ -0,0 +1,13 @@
+console.log('Person')
+
+const isAdult = (age) => age >= 18
+
+const canDrink = (age) => age >= 18
+
+const isSenior = (age) => age >= 65
+
+export {
+    isAdult,
+    canDrink,
+    isSenior as default
+}
